ad05773be7ba3722b1068565905fe29461a7844e
shahzahandev <shahzahansiraj516@gmail.com>
2026-05-30 02:07:41 +0600

    dbConnection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
 
 let dbConnection = () => {
-   mongoose.connect(process.env.DB_URL).then(() => {
+   mongoose.connect(process.env.DB_URL)
+   .then(() => {
     console.log('MongoDB Connected.')
+   })
+   .catch((error) => {
+      console.log('MongoDB Connection failed.')
    })
 }
 
--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const dbConnection = require('./config/dbConnection')
 const {patientProfileController, getAllProfile, singleProfile, setHoldProfile, deleteProfile, updateProfile, allProfileWithoutHold, getHoldProfile} = require('./controller/patientProfleController')
 const app = express()
 
-app.use(express.json())
-const port = process.env.PORT || 5000
 
+// middleware
+app.use(express.json({
+    limit: '10kb'
+}))
+
+// MongoDB Connection
 dbConnection()
 
+// Auth controllers
 app.post('/registration', registrationController)
 app.post('/login', loginController)
 app.post('/logout', LogoutController)
+
+
 //==========================================
 app.post('/profile', patientProfileController)
 app.get('/allProfile', getAllProfile)
@@ -23,6 +30,8 @@ app.post("/updateProfile/:id", updateProfile)
 app.get("/withoutHold", allProfileWithoutHold)
 app.get("/allHoldProfile", getHoldProfile)
 
+// port
+const port = process.env.PORT || 5000
 
 app.listen(port, (req, res) => {
     console.log(`Server is running on port ${port}`);    
